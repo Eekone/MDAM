9f276534a041b9a1e5bc93055a10612cff85d2f6
Pavel Khaustov <pkcrescendomagnolia@gmail.com>
2018-01-20 22:30:45 +0300

    BKM1 unit tests implementation

--- Application/src/test/java/com/texnoprom/mdam/BKM1DataParsingTest.java
+++ Application/src/test/java/com/texnoprom/mdam/BKM1DataParsingTest.java
@@ -6,7 +6,6 @@ import com.google.gson.JsonParser;
 import com.texnoprom.mdam.activities.Modbus;
 import com.texnoprom.mdam.models.BTRegister;
 
-import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
 import java.io.InputStream;
@@ -14,12 +13,17 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-
 public class BKM1DataParsingTest {
+
     private static byte valuesInput[] = {1, 3, 20, 0, 0, 0, 1, 0, 0, 0, 1, 3, -56, 3, -56, 3, -56, 0, 31, 1, -98, 3, -56, 24, -76};
-    private static byte configInput[] = {1, 3, 20, 0, 0, 0, 1, 0, 0, 0, 1, 3, -56, 3, -56, 3, -56, 0, 31, 1, -98, 3, -56, 24, -76};
-    private static byte calibrationInput[] = {1, 3, 20, 0, 0, 0, 1, 0, 0, 0, 1, 3, -56, 3, -56, 3, -56, 0, 31, 1, -98, 3, -56, 24, -76};
-    private static byte miscInput[] = {1, 3, 20, 0, 0, 0, 1, 0, 0, 0, 1, 3, -56, 3, -56, 3, -56, 0, 31, 1, -98, 3, -56, 24, -76};
+    private static byte configInput[] = {1, 3, 10, 0, 1, 37, -128, 0, 0, 0, 0, 0, 0, 106, -55};
+    private static byte calibrationInput[] = {1, 3, 30, 0, 0, 0, 28, 1, -12, 0, 0, 0, 78, 1, -12, 0, 0, 0, 78, 1, -12, 0, 0, 1, 37, 0, 101, 0, 0, 0, 11, 11, -72, -90, -97};
+    private static byte miscInput[] = {1, 3, 22, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, -31, 0, 1, 0, 1, 0, 0, 0, -56, -84, 46};
+
+    static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
 
     @Test
     public void valuesParsing_isCorrect() throws Exception {
@@ -49,7 +53,7 @@ public class BKM1DataParsingTest {
 
         InputStream inputStream = this.getClass()
                 .getClassLoader().getResourceAsStream(expectedFilePath);
-        JsonElement expected = jsonParser.parse(IOUtils.toString(inputStream));
+        JsonElement expected = jsonParser.parse(convertStreamToString(inputStream));
 
         assertEquals(expected, actual);
     }
